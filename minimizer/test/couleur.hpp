7c38cd12ed287497529629c9deb128034b54a556
chiboubnour <chiboubnour32@gmail.com>
2025-11-30 18:45:14 +0100

    Create couleur.hpp

---
+++ minimizer/test/couleur.hpp
@@ -0,0 +1,33 @@
+//!
+//! @file 				BitRandom.hpp
+//! @author 			Bertrand LE GAL <bertrand.legal@ims-bordeaux.fr>
+//! @created 			2022
+//!
+//!     Copyright (c) 2022, Bertrand LE GAL
+//!     All rights reserved.
+//!     
+//!     Redistribution and use in source and binary forms, with or without
+//!     modification, are not permitted with written authorization.
+//!     
+//!
+#ifndef COULEUR
+#define COULEUR
+
+#include <iostream>
+#include <complex>
+#include <iostream>
+
+using namespace std;
+
+#define KNRM  "\x1B[0m"
+#define KRED  "\x1B[31m"
+#define KGRN  "\x1B[32m"
+#define KYEL  "\x1B[33m"
+#define KBLU  "\x1B[34m"
+#define KMAG  "\x1B[35m"
+#define KCYN  "\x1B[36m"
+#define KWHT  "\x1B[37m"
+
+
+
+#endif
