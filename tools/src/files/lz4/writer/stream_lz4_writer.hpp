5455185870ddf883c0b8076dc776b68fcdb07ef9
chiboubnour <chiboubnour32@gmail.com>
2025-12-02 17:30:32 +0100

    Create stream_lz4_writer.hpp

---
+++ tools/src/files/lz4/writer/stream_lz4_writer.hpp
@@ -0,0 +1,20 @@
+#pragma once
+#include "../../stream_writer.hpp"
+#include "../../../colors/colors.hpp"
+#include "../../../libs/lz4/lz4file.h"
+
+class stream_lz4_writer : public stream_writer
+{
+private:
+    FILE*             stream;
+    LZ4_writeFile_t* lz4fWrite;
+
+public:
+     stream_lz4_writer(const std::string& filen);
+     stream_lz4_writer(const char*       filen);
+    ~stream_lz4_writer();
+
+    virtual bool is_open();
+    virtual int  write  (void* buffer, int eSize, int eCount);
+    virtual void close  ();
+};
