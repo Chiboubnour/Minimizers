7c4b13f15d02a9d06e0369c6c266e092703fb212
chiboubnour <chiboubnour32@gmail.com>
2026-02-04 11:01:34 +0100

    Update hach_minimizer_hls.cpp

--- hach_minimizer/hls/hach_minimizer_hls.cpp
+++ hach_minimizer/hls/hach_minimizer_hls.cpp
@@ -1,8 +1,13 @@
 #include <cstdint>
+#include <ap_int.h>
+#include <hls_stream.h>
 
 #define HASH_WIDTH 64
 
-inline uint64_t bfc_hash_64(uint64_t key, uint64_t mask) {
+inline ap_uint<64> bfc_hash_64(ap_uint<64> key) {
+#pragma HLS INLINE
+    ap_uint<64> mask = ~0ULL;
+
     key = (~key + (key << 21)) & mask;
     key = key ^ (key >> 24);
     key = ((key + (key << 3)) + (key << 8)) & mask;
@@ -10,42 +15,32 @@ inline uint64_t bfc_hash_64(uint64_t key, uint64_t mask) {
     key = ((key + (key << 2)) + (key << 4)) & mask;
     key = key ^ (key >> 28);
     key = (key + (key << 31)) & mask;
+
     return key;
 }
 
-void parallel_hash_calc(uint64_t input_data, uint64_t& output_data) {
-    uint64_t mask = ~0ULL;
-    output_data = bfc_hash_64(input_data, mask);
-}
-
 void minimizer(
-    const uint64_t* input_minimizers,
-    uint64_t*       output_hashes,
-    unsigned int    data_size_words
+    const ap_uint<64>* input_minimizers,
+    ap_uint<64>*       output_hashes,
+    unsigned int       data_size_words
 ) {
+
 #ifdef _PRAGMA_HLS_
-    #pragma HLS INTERFACE m_axi port=input_minimizers offset=slave bundle=gmem_in max_read_burst_length=256
-    #pragma HLS INTERFACE m_axi port=output_hashes offset=slave bundle=gmem_out max_write_burst_length=256
-    #pragma HLS INTERFACE s_axilite port=data_size_words
-    #pragma HLS INTERFACE s_axilite port=return
+#pragma HLS INTERFACE m_axi port=input_minimizers offset=slave bundle=gmem_in  max_read_burst_length=256
+#pragma HLS INTERFACE m_axi port=output_hashes   offset=slave bundle=gmem_out  max_write_burst_length=256
+#pragma HLS INTERFACE s_axilite port=data_size_words
+#pragma HLS INTERFACE s_axilite port=return
 #endif
 
-    const unsigned int BURST_SIZE = 1024;
+    main_loop:
+    for (unsigned int i = 0; i < data_size_words; i++) {
 
-    for (unsigned int burst_idx = 0; burst_idx < data_size_words; burst_idx += BURST_SIZE) {
-        unsigned int remaining = data_size_words - burst_idx;
-        unsigned int current_burst = (remaining < BURST_SIZE) ? remaining : BURST_SIZE;
-
-        for (unsigned int word_idx = 0; word_idx < current_burst; word_idx++) {
 #ifdef _PRAGMA_HLS_
-            #pragma HLS PIPELINE II=1
+        #pragma HLS PIPELINE II=1
 #endif
-            unsigned int idx = burst_idx + word_idx;
-            uint64_t input_data = input_minimizers[idx];
-            uint64_t output_data;
 
-            parallel_hash_calc(input_data, output_data);
-            output_hashes[idx] = output_data;
-        }
+        ap_uint<64> in  = input_minimizers[i];
+        ap_uint<64> out = bfc_hash_64(in);
+        output_hashes[i] = out;
     }
 }
