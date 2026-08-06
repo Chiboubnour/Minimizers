61bf24b60b94851ff639a5f79b3adec8a23a8531
chiboubnour <chiboubnour32@gmail.com>
2026-08-06 23:38:13 +0200

    Create minimizer.hpp

---
+++ minimizer/hls/final bis /minimizer.hpp
@@ -0,0 +1,30 @@
+#ifndef MINIMIZER_64BITS_2_PARAMS_HPP
+#define MINIMIZER_64BITS_2_PARAMS_HPP
+
+constexpr int PAR_FACTOR  = 16;
+constexpr int SMER_SIZE   = 31;
+constexpr int WINDOW_SIZE = 19;
+constexpr int SMER_BITS   = 2 * SMER_SIZE;
+
+
+constexpr int MEM_WIDTH = 8 * PAR_FACTOR;
+
+static_assert(MEM_WIDTH % (8 * PAR_FACTOR) == 0,
+              "MEM_WIDTH doit etre un multiple de 8*PAR_FACTOR");
+constexpr int MEM_RATIO = MEM_WIDTH / (8 * PAR_FACTOR);
+
+constexpr int MAX_BASES_HW = 1000000;
+
+
+constexpr int COSIM_MAX_BASES = MAX_BASES_HW;
+constexpr int COSIM_MAX_WORDS = (COSIM_MAX_BASES + PAR_FACTOR - 1) / PAR_FACTOR;
+constexpr int COSIM_MAX_BEATS = (COSIM_MAX_WORDS + MEM_RATIO - 1) / MEM_RATIO;
+
+constexpr int GROUP_W       = 2;
+constexpr int GROUP_BITS    = 64 * GROUP_W;
+constexpr int COSIM_MAX_HASHGROUP = (COSIM_MAX_BASES + GROUP_W - 1) / GROUP_W;
+
+
+constexpr int AXIS_BITS = 64 * PAR_FACTOR + 8;
+
+#endif
